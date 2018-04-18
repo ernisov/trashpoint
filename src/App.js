e03ad4da8d06e1a332c12b41a5121f420ca38e20
Sultan Ernisov <ernisov@gmail.com>
2018-04-18 21:22:24 +0600

    ignored vscoed

--- src/App.js
+++ src/App.js
@@ -7,27 +7,27 @@ import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
 class App extends Component {
-    componentWillMount() {
-        const config = {
-            apiKey: 'AIzaSyB8JlsGOkUcRnG4M3D3GiJ9yDeqNJ_hX9o',
-            authDomain: 'taza-jasho.firebaseapp.com',
-            databaseURL: 'https://taza-jasho.firebaseio.com',
-            projectId: 'taza-jasho',
-            storageBucket: 'taza-jasho.appspot.com',
-            messagingSenderId: '1033397887766'
-        };
-        firebase.initializeApp(config);
-    }
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyB8JlsGOkUcRnG4M3D3GiJ9yDeqNJ_hX9o',
+      authDomain: 'taza-jasho.firebaseapp.com',
+      databaseURL: 'https://taza-jasho.firebaseio.com',
+      projectId: 'taza-jasho',
+      storageBucket: 'taza-jasho.appspot.com',
+      messagingSenderId: '1033397887766'
+    };
+    firebase.initializeApp(config);
+  }
 
-    render() {
-        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
-        return (
-            <Provider store={store}>
-                <Router />
-            </Provider>
-        );
-    }
+    return (
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    );
+  }
 }
 
 export default App;
